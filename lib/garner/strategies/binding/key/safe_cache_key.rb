ec9f0f40e644b9675879a5e5507a43d828223e27
Radu Busuioc <ra.busuioc@gmail.com>
2014-05-09 11:53:30 +0100

    trimming extra zeros from cache key timestamps

--- lib/garner/strategies/binding/key/safe_cache_key.rb
+++ lib/garner/strategies/binding/key/safe_cache_key.rb
@@ -4,6 +4,7 @@ module Garner
       module Key
         class SafeCacheKey < Base
           VALID_FORMAT = /^(?<model>[^\/]+)\/(?<id>.+)-(?<timestamp>[0-9]{14})$/
+          RAILS_4_FORMAT = /^(?<model>[^\/]+)\/(?<id>.+)-(?<timestamp>[0-9]{23})$/
 
           # Compute a cache key from an object binding. Only return a key if
           # :cache_key and :updated_at are both defined and present on the
@@ -21,11 +22,16 @@ module Garner
             return unless binding.respond_to?(:updated_at) && binding.updated_at
 
             # Check for ActiveModel cache key format
-            return unless binding.cache_key =~ VALID_FORMAT
+            if binding.cache_key =~ VALID_FORMAT
+              cache_key = binding.cache_key
+            else
+              return unless binding.cache_key =~ RAILS_4_FORMAT
+              cache_key = binding.cache_key[0...-9]
+            end
 
             decimal_portion = binding.updated_at.utc.to_f % 1
             decimal_string = sprintf("%.10f", decimal_portion).gsub(/^0/, "")
-            "#{binding.cache_key}#{decimal_string}"
+            "#{cache_key}#{decimal_string}"
           end
 
         end
